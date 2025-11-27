5b27a34d5275d20aa3cfb09cedc79c4fb25d76af
syeda maham amjad <zohameer96@gmail.com>
2025-11-27 21:50:27 +0500

    changed ui and added dark theme functionality

--- class17/airfore-share/src/components/Button.jsx
+++ class17/airfore-share/src/components/Button.jsx
@@ -1,7 +1,39 @@
 import './index.scss'
-function MainBtn({disabled,title, onClick}){
-    return(
-        <button className='main-btn' style={{ color: disabled ? "rgba(186, 179, 179, 0.56)" : "#4a4a4a", border: disabled ? "1px solid rgba(186, 179, 179, 0.56)" : "1px solid #4a4a4a" }} disabled={disabled} onClick={onClick} >{title}</button>
-    )
+
+function MainBtn({ disabled, title, theme }) {
+  // Define colors based on theme
+  const colors = {
+    light: {
+      text: "#4a4a4a",
+      border: "#4a4a4a",
+      background:"transparent"
+    },
+    dark: {
+      text: "#fff",
+      border: "#fff",
+      background:"transparent"
+
+    },
+
+  };
+
+  const currentColors = colors[theme] || colors.light;
+
+  return (
+    <button
+      className="main-btn"
+      style={{
+        backgroundColor:currentColors.background,
+        color: disabled ? "rgba(186, 179, 179, 0.56)" : currentColors.text,
+        border: disabled
+          ? "1px solid rgba(186, 179, 179, 0.56)"
+          : `1px solid ${currentColors.border}`,
+      }}
+      disabled={disabled}
+    >
+      {title}
+    </button>
+  );
 }
-export default MainBtn 
+
+export default MainBtn;
